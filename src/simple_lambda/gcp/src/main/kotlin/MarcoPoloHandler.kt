1c994ba292cc699182fb31bb23c93bdb60882793
Michael van Niekerk <mvniekerk@chazaqdev.com>
2019-09-24 13:22:26 +0200

    Azure Functions

--- src/simple_lambda/gcp/src/main/kotlin/MarcoPoloHandler.kt
+++ src/simple_lambda/gcp/src/main/kotlin/MarcoPoloHandler.kt
@@ -4,9 +4,11 @@ class MarcoRequest {
     lateinit var name: String
 }
 
+data class PoloResponse(val name: String)
+
 fun main(args: Array<String>) {
     exports.marcoPolo = { request: dynamic, response: dynamic ->
         val input = request.body.unsafeCast<MarcoRequest>()
-        response.status(200).send("MarcoPolo ${input.name}")
+        response.status(200).send(PoloResponse("MarcoPolo ${input.name}"))
     }
 }
